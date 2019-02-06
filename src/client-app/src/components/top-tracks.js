49012a5b5e97adc925d1fb8dce6b8cab5beaea0f
rileykacmarynski <riley.kacmarynski@gmail.com>
2019-02-05 18:35:16 -0600

    Fix api

---
+++ src/client-app/src/components/top-tracks.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import List from '@material-ui/core/List';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import styled from 'styled-components';
+
+import { fetchAsync } from '../code/helpers';
+import TrackListItem from './track-list-item';
+
+const TrackList = styled.div`
+  padding-top: 10px;
+  h3 {
+    text-align: center;
+    padding-top: 10px;
+  }
+`;
+
+class TopTracks extends Component {
+  state = {
+    tracks: []
+  };
+
+  async componentDidMount() {
+    const res = await fetchAsync('tracks');
+    this.setState({ tracks: res.value });
+  }
+
+  render() {
+    return (
+      <TrackList>
+        <Grid container spacing={16}>
+          <Grid item xs={3} />
+          <Grid item xs={6}>
+            <Paper>
+              <Typography variant="h3" color="primary">
+                Top Tracks
+              </Typography>
+              <div>
+                <List>
+                  {this.state.tracks.map(track => (
+                    <TrackListItem track={track} key={track.trackId} />
+                  ))}
+                </List>
+              </div>
+            </Paper>
+            <Grid item xs={3} />
+          </Grid>
+        </Grid>
+      </TrackList>
+    );
+  }
+}
+
+export default TopTracks;
